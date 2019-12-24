eafe0eefbda1f9e0bea0ac6ae06746c141a10874
samuel950523 <samuel95@naver.com>
2019-12-24 14:37:43 +0900

    1224

--- e1224/p1224_3.py
+++ e1224/p1224_3.py
@@ -1,1 +1,11 @@
-# indexing
+# indexing & slicing
+sen = "Life is too short"
+print('sen[3] = ', sen[3])
+print('sen[:3] = ', sen[:3])
+print('sen[-1] = ', sen[-1])
+
+print('len(sen) = ', len(sen))
+print('len(sen[:3]) = ', len(sen[:3]))
+print('len(sen[-1]) = ', len(sen[-1]))
+
+print('sen[:4], sen[-5:] = ', sen[:4], sen[-5:])
