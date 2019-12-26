ee59a76b2606f70c18c14ae5871909bc575817c4
samuel950523 <samuel95@naver.com>
2019-12-26 09:28:58 +0900

    1226

---
+++ p1226/p1226_1.py
@@ -0,0 +1,21 @@
+# 리스트[] 튜플() 딕셔너리{} 집합{}
+
+# 리스트[]
+fruits = ['사과', '바나나', '포도']
+print(fruits)
+
+# 빈 리스트
+movieList = []
+print(movieList)
+
+# 서로 다른 데이터형으로 리스트 정의
+myList = [True, 100, '홍길동']
+print(f'myList = {myList}')
+
+# 리스트 인덱싱 / 슬라이싱
+print(fruits[0], fruits[-1])
+# 리스트이름[start:end:step])
+print(fruits[:1])
+print(fruits[0:1])
+print(fruits[0::2])
+print(fruits[::])
