0eadc53ed505053a638570a04d4cf6d18179e8f4
samuel950523 <samuel95@naver.com>
2020-01-09 10:50:54 +0900

    create table

--- mysql_practice/d0109/d0109_1.sql
+++ mysql_practice/d0109/d0109_1.sql
@@ -60,3 +60,67 @@ CREATE TABLE buyTbl
 );
 SHOW TABLES;
 SELECT * FROM buyTbl;
+
+-- userTbl 테이블의 레코드 삽입 
+-- INSERT INTO userTbl VALUES('LSG', '이승기', 1987, '서울', '011', '1111111', 182, '2008-8-8');
+-- INSERT INTO userTbl VALUES('KBS', '김범수', 1979, '경남', '011', '2222222', 173, '2012-4-4');
+-- INSERT INTO userTbl VALUES('KKH', '김경호', 1971, '전남', '019', '3333333', 177, '2007-7-7');
+-- INSERT INTO userTbl VALUES('JYP', '조용필', 1950, '경기', '011', '4444444', 166, '2009-4-4');
+-- INSERT INTO userTbl VALUES('SSK', '성시경', 1979, '서울', NULL  , NULL      , 186, '2013-12-12');
+-- INSERT INTO userTbl VALUES('LJB', '임재범', 1963, '서울', '016', '6666666', 182, '2009-9-9');
+-- INSERT INTO userTbl VALUES('YJS', '윤종신', 1969, '경남', NULL  , NULL      , 170, '2005-5-5');
+-- INSERT INTO userTbl VALUES('EJW', '은지원', 1972, '경북', '011', '8888888', 174, '2014-3-3');
+-- INSERT INTO userTbl VALUES('JKW', '조관우', 1965, '경기', '018', '9999999', 172, '2010-10-10');
+-- INSERT INTO userTbl VALUES('BBK', '바비킴', 1973, '서울', '010', '0000000', 176, '2013-5-5');
+
+-- buyTbl 테이블의 레코드 삽입 
+-- INSERT INTO buyTbl VALUES(NULL, 'KBS', '운동화', NULL   , 30,   2);
+-- INSERT INTO buyTbl VALUES(NULL, 'KBS', '노트북', '전자', 1000, 1);
+-- INSERT INTO buyTbl VALUES(NULL, 'JYP', '모니터', '전자', 200,  1);
+-- INSERT INTO buyTbl VALUES(NULL, 'BBK', '모니터', '전자', 200,  5);
+-- INSERT INTO buyTbl VALUES(NULL, 'KBS', '청바지', '의류', 50,   3);
+-- INSERT INTO buyTbl VALUES(NULL, 'BBK', '메모리', '전자', 80,  10);
+-- INSERT INTO buyTbl VALUES(NULL, 'SSK', '책'    , '서적', 15,   5);
+-- INSERT INTO buyTbl VALUES(NULL, 'EJW', '책'    , '서적', 15,   2);
+-- INSERT INTO buyTbl VALUES(NULL, 'EJW', '청바지', '의류', 50,   1);
+-- INSERT INTO buyTbl VALUES(NULL, 'BBK', '운동화', NULL   , 30,   2);
+-- INSERT INTO buyTbl VALUES(NULL, 'EJW', '책'    , '서적', 15,   1);
+-- INSERT INTO buyTbl VALUES(NULL, 'BBK', '운동화', NULL   , 30,   2);
+
+-- 테이블 조회
+SELECT * FROM userTbl;
+SELECT * FROM buyTbl;
+SELECT COUNT(*) FROM userTbl;  -- 10
+SELECT COUNT(*) FROM buyTbl;  -- 12
+
+/* ------------------------------ */
+-- FILTERING : 조건에 맞는 레코드 출력하기
+-- WHERE 조건절의 연산자는? 관계연산자(>, = , ...), 논리연산자(AND, OR NOT)
+-- userTbl 테이블에서 김경호만 출력하기
+SELECT * FROM userTbl WHERE name = '김경호';
+-- userTbl 테이블에서 birthYear가 1970보다 크거나 같고 height이 182보다 작다
+SELECT * FROM userTbl WHERE birthYear >= 1970 AND height < 182;
+
+-- userTbl 테이블에서 180<=height<=183 인 userId, name 출력하기
+SELECT userID, name FROM userTbl WHERE height>=180 AND height<=183;
+
+-- 범위를 지정할 때 사용하는 연산자 BETWEEN
+-- BETWEEN 값1 AND 값2 : 값1 ~ 값2
+SELECT userID, name FROM userTbl WHERE height BETWEEN 180 AND 183;
+SELECT COUNT(*) FROM userTbl WHERE height BETWEEN 180 AND 183; -- 2
+
+-- 해당 값에 해당하는 레코드 추출 : IN 연산자
+-- 컬럼명 IN(값1, 값2 ...)
+-- userTbl 에서 addr 컬럼값이 경남, 전남, 경북인 레코드 출력
+SELECT COUNT(*) FROM userTbl WHERE addr = '경남' OR addr = '전남' OR addr = '경북'; -- 4
+SELECT COUNT(*) FROM userTbl WHERE addr IN ('경남','전남','경북'); -- 4
+
+-- LIKE 연산자
+-- 문자열이나 값 검색
+-- LIKE ..%( )
+-- LIKE _( )
+SELECT COUNT(*) FROM userTbl WHERE name LIKE '김%'; -- 2
+SELECT COUNT(*) FROM userTbl WHERE name LIKE '김__'; -- 2
+SELECT COUNT(*) FROM userTbl WHERE name LIKE '김_'; -- 0   _ 의 갯수에 따라 달라짐
+SELECT * FROM userTbl WHERE name LIKE '_종신'; -- 윤종신
+
